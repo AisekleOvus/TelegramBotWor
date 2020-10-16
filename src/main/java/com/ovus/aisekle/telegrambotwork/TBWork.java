1535a99e1dca0e57ae604a2f8597e30b76bd8bd0
AisekleOvus <aisekleovus@protonmail.ch>
2020-10-16 16:22:59 +0300

    For now the only one method - sendPhoto :)

--- src/main/java/com/ovus/aisekle/telegrambotwork/TBWork.java
+++ src/main/java/com/ovus/aisekle/telegrambotwork/TBWork.java
@@ -11,22 +11,21 @@ public class TBWork {
 	private String token;
 	private static final String URL = "https://api.telegram.org/bot%s/%s";
 	private String chat_id;
-	private String method;
+
 		
-	public TBWork(String token, String chat_id, String method) {
+	public TBWork(String token, String chat_id) {
 		this.token = token;
 		this.chat_id = chat_id;
-		this.method = method;
 	}
 	
     public String sendPhoto(String params) {
+        String method = "sendPhoto";
     	String result = "empty";
     	String wholeURL = String.format(URL, token, method + "?chat_id=" + chat_id + params); // must be pair of '&key=value'
     	Connection telegramBotConnection = Jsoup.connect(wholeURL).ignoreContentType(true);
     	try {
     		result = telegramBotConnection.execute().body();
-    		System.out.println(result);
-    	    		}catch(Exception e) {
+    	}catch(Exception e) {
     		e.printStackTrace();
     	}
     	return result;
